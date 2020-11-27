c7e38b84968909be56e4a9c824bb6e5a096c0c4f
Patryk Nowak <papknight@gmail.com>
2020-11-27 18:55:08 +0100

    change small --> big mistake, get out from loop

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -14,9 +14,10 @@ def guess():
     computer = random.randint(1, 100)
     while user_answer != computer:
         if computer > user_answer:
-            print("To big")
-        else:
             print("To small")
+        else:
+            print("To big")
+        user_answer = get()
     print("You win")
 
 guess()
